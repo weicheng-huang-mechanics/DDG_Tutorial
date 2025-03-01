b608db07fbb154c3e0f433b1be77cf0b7a4e0218
Weicheng Huang <164727992+weicheng-huang-mechanics@users.noreply.github.com>
2025-03-01 17:56:36 +0000

    Create main.m

---
+++ mass_spring_system/single_DOF/main.m
@@ -0,0 +1,89 @@
+% DDG tutorial, single DOF system 
+% Weicheng Huang, weicheng.huang@ncl.ac.uk
+
+clear all;
+close all;
+clc;
+
+% Define struct
+systemParams = struct();
+
+% Mass
+systemParams.m = 1.0;
+
+% Spring stiffess
+systemParams.k = 10; 
+
+% Viscosity
+systemParams.c = 0.1; 
+
+% Original length of spring
+systemParams.l0 = 1.0;
+
+% Driving amplitude
+systemParams.F0 = 1.0;
+
+% Driving frequency
+systemParams.omega = 1; % driving frequency
+
+% Define struct
+simParams = struct();
+
+% Total time
+simParams.totalTime = 10;
+
+% Time step size
+simParams.dt = 1e-3; 
+
+% Tolerance
+simParams.eps = 1e-6;
+
+% Total step
+simParams.Nsteps = round(simParams.totalTime / simParams.dt);
+
+% Initialize 
+t = linspace(0, simParams.totalTime, simParams.Nsteps);
+x = zeros(simParams.Nsteps, 1); 
+u = zeros(simParams.Nsteps, 1); 
+
+% Initial condition
+x(1) = 1.0;
+u(1) = 0.0;
+
+% Simulation loop
+for k=1:simParams.Nsteps-1 
+    
+    % update DOF
+    x_old = x(k);
+    u_old = u(k);    
+    
+    % update time
+    t_new = t(k+1);
+    
+    % Initial guess
+    x_new = x_old + u_old * simParams.dt; 
+    
+    % Initialize error to a large value
+    err = simParams.eps * 100; 
+    
+    % Newton's solver
+    while err > simParams.eps
+        
+        [force, jacob] = getF(x_new, x_old, u_old, t_new, systemParams, simParams);
+        deltaX = force / jacob;
+        
+        x_new = x_new - deltaX;
+        
+        err = abs(force);
+    end
+    
+    u_new = (x_new - x_old) / simParams.dt;
+    
+    x(k+1) = x_new; % store solution
+    u(k+1) = u_new;
+end
+
+% plot result
+plot(t, x, 'k-'); 
+xlabel('t')
+ylabel('x')
