a274a5d6ff43a90c5c55cbf3ec5f54b7d68a7d4d
Weicheng Huang <164727992+weicheng-huang-mechanics@users.noreply.github.com>
2025-03-01 19:05:53 +0000

    Add files via upload

---
+++ soft_robot/case_1/InitialStretchingElement.m
@@ -0,0 +1,35 @@
+% build stretching element
+function sElement = InitialStretchingElement(rodParams, edge)
+
+    EA = rodParams.EA;
+    x = rodParams.x;
+    ne = rodParams.ne;
+    
+    sElement = struct('nodeIndex', {}, 'globalIndex', {}, 'refLen', {}, 'EA_local', {}, ... 
+        'start_node_1', {}, 'start_node_2', {}, 'Br', {});
+    
+    for i = 1:ne
+        sElement(i).nodeIndex = edge(i,:);
+        
+        % set global index
+        sElement(i).globalIndex = zeros(4,1);
+        
+        sElement(i).globalIndex(1) = 2 * (sElement(i).nodeIndex(1) - 1) + 1;
+        sElement(i).globalIndex(2) = 2 * (sElement(i).nodeIndex(1) - 1) + 2;
+        
+        sElement(i).globalIndex(3) = 2 * (sElement(i).nodeIndex(2) - 1) + 1;
+        sElement(i).globalIndex(4) = 2 * (sElement(i).nodeIndex(2) - 1) + 2;
+         
+        node_1 = getVertex(x, sElement(i).nodeIndex(1));
+        node_2 = getVertex(x, sElement(i).nodeIndex(2));
+        
+        sElement(i).refLen = norm(node_2 - node_1); 
+        
+        sElement(i).EA_local = EA;
+        
+        sElement(i).start_node_1 = node_1;
+        sElement(i).start_node_2 = node_2;
+        sElement(i).Br = [1e3; 0.0];        
+    end
+    
+end
