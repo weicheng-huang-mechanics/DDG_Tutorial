80a3ae491bceb2140148b4da4e377e7fb902f1ce
Weicheng Huang <164727992+weicheng-huang-mechanics@users.noreply.github.com>
2025-03-04 16:17:41 +0000

    Update stretchingForce.m

--- 3d_surface/case_1/stretchingForce.m
+++ 3d_surface/case_1/stretchingForce.m
@@ -4,14 +4,14 @@ edge = (node1 - node0);
 edgeLen = norm(edge);
 tangent = edge / edgeLen;
 epsX = edgeLen/l_k - 1;
-dF_unit = EA * tangent * epsX;
+dF_unit = EA * tangent * epsX * l_k * l_k;
 
 dF = zeros(6,1);
 dF(1:3) = - dF_unit;
 dF(4:6) =   dF_unit;
 
 Id3 = eye(3);
-M = EA * ( (1/l_k - 1/edgeLen) * Id3 + 1/edgeLen * (edge*edge')/ edgeLen^2 );
+M = EA * l_k * l_k * ( (1/l_k - 1/edgeLen) * Id3 + 1/edgeLen * (edge*edge')/ edgeLen^2 );
 
 dJ = zeros(6,6);
 dJ(1:3, 1:3) = M;
