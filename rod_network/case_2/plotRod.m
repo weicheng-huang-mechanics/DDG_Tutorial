c5aae2697c8cc0a5a7bdf1abd01eaf8b11252b9c
Weicheng Huang <164727992+weicheng-huang-mechanics@users.noreply.github.com>
2025-04-07 19:49:59 +0100

    Update plotRod.m

--- rod_network/case_2/plotRod.m
+++ rod_network/case_2/plotRod.m
@@ -4,7 +4,7 @@ function plotRod(x, nv, sElement)
 %   Input:
 %       rodParams - the defined rod struct contains the physical and
 %                   numerical parameters of the simulated system
-%       sElement - the bending element list (ns x 2)
+%       sElement - the bending element list (ne x 2)
 
 x1 = x(1:3:3*nv-2);
 x2 = x(2:3:3*nv-1);
