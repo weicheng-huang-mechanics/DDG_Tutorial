988545eb190c824c14647f2c2c09aaab6b19388c
Weicheng Huang <164727992+weicheng-huang-mechanics@users.noreply.github.com>
2025-04-07 19:30:50 +0100

    Update getFb.m

--- 3d_surface/case_2/getFb.m
+++ 3d_surface/case_2/getFb.m
@@ -1,9 +1,8 @@
 function [Fb, Jb] = getFb(plateParams, bElement)
-% This function computes the bending force and jacobian of the simulated 
-% system. In this case the simulated system is a plate
+% This function computes the bending force and jacobian of the simulated system.
 % INPUTS: plateParams - the defined plate struct contains the physical and
 %                     numerical parameters of the simulated system
-%         bElement - the bending element list (nb x 3)
+%         bElement - the bending element list
 %
 % OUTPUTS: Fb - bending forces (ndof x 1)
 %          Jb - bending jacobian (ndof x ndof)
