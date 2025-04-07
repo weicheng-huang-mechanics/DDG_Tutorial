ea5d94bfe1c5f90e9c6da764c6bfc3317a053bcb
Weicheng Huang <164727992+weicheng-huang-mechanics@users.noreply.github.com>
2025-04-07 19:48:50 +0100

    Update getFb.m

--- rod_network/case_2/getFb.m
+++ rod_network/case_2/getFb.m
@@ -1,9 +1,8 @@
 function [Fb, Jb] = getFb(rodParams, bElement)
-% This function computes the bending force and jacobian of the simulated 
-% system. In this case the simulated system is a 3D helix
+% This function computes the bending force and jacobian of the simulated system.
 % INPUTS: rodParams - the defined rod struct contains the physical and
 %                     numerical parameters of the simulated system
-%         bElement - the bending element list (nb x 3)
+%         bElement - the bending element list
 %
 % OUTPUTS: Fb - bending forces (ndof x 1)
 %          Jb - bending jacobian (ndof x ndof)
