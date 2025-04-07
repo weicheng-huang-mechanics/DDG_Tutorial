a6dd3f01f8903ac22fd770b01812e1285ec89017
Weicheng Huang <164727992+weicheng-huang-mechanics@users.noreply.github.com>
2025-04-07 19:24:22 +0100

    Update getFt.m

--- 3d_curve/case_1/getFt.m
+++ 3d_curve/case_1/getFt.m
@@ -1,6 +1,5 @@
 function [Ft, Jt] = getFt(rodParams, bElement)
-% This function computes the twisting force and jacobian of the simulated 
-% system. In this case the simulated system is a 3D helix
+% This function computes the twisting force and jacobian of the simulated system. 
 % INPUTS: rodParams - the defined rod struct contains the physical and
 %                     numerical parameters of the simulated system
 %         bElement - the bending element list
