f1baab5ecb1a284c04d88b67aecbf8ebefa44577
Weicheng Huang <164727992+weicheng-huang-mechanics@users.noreply.github.com>
2025-04-07 19:57:26 +0100

    Update getFs.m

--- soft_robot/case_1/getFs.m
+++ soft_robot/case_1/getFs.m
@@ -1,9 +1,8 @@
 function [Fs, Js] = getFs(rodParams, sElement)
-% This function computes the stretching force and jacobian of the simulated 
-% system. In this case the simulated system is a 2D beam
+% This function computes the stretching force and jacobian of the simulated system.
 % INPUTS: rodParams - the defined rod struct contains the physical and
 %                     numerical parameters of the simulated system
-%         sElement - the stretching element list (ne x 2)
+%         sElement - the stretching element list
 %
 % OUTPUTS: Fs - stretching forces (ndof x 1)
 %          Js - stretching jacobian (ndof x ndof)
