8f0ef012623e0565ce1ec4eebf82e24676cf8935
Weicheng Huang <164727992+weicheng-huang-mechanics@users.noreply.github.com>
2025-04-07 19:58:44 +0100

    Update getFb.m

--- soft_robot/case_2/getFb.m
+++ soft_robot/case_2/getFb.m
@@ -1,9 +1,8 @@
 function [Fb, Jb] = getFb(rodParams, bElement)
-% This function computes the bending force and jacobian of the simulated 
-% system. In this case the simulated system is a 2D beam
+% This function computes the bending force and jacobian of the simulated system.
 % INPUTS: rodParams - the defined rod struct contains the physical and
 %                     numerical parameters of the simulated system
-%         bElement - the bending element list (nb x 3)
+%         bElement - the bending element list (nb x 2)
 %
 % OUTPUTS: Fb - bending forces (2*nv x 1)
 %          Jb - bending jacobian (2*nv x 2*nv)
