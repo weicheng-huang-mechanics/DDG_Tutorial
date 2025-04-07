cdb433e75a764c18026c10b311d1852ecdf3c926
Weicheng Huang <164727992+weicheng-huang-mechanics@users.noreply.github.com>
2025-04-07 20:46:30 +0100

    Update main.m

--- rod_network/case_1/main.m
+++ rod_network/case_1/main.m
@@ -1,6 +1,7 @@
 %% DDG tutorial, Net & gridshell, Case 1: Deflection of a flexible net under gravity
 % Weicheng Huang, weicheng.huang@ncl.ac.uk
 % Dezhong Tong, dezhong@umich.edu
+% Zhuonan Hao, znhao@g.ucla.edu
 
 clear all;
 close all;
