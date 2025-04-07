1e1e9267511aa57beebb51814e4d5a7a53ebf2c6
Weicheng Huang <164727992+weicheng-huang-mechanics@users.noreply.github.com>
2025-04-07 20:22:21 +0100

    Update defConsParams.m

--- 2d_surface/case_1/defConsParams.m
+++ 2d_surface/case_1/defConsParams.m
@@ -3,10 +3,10 @@ function consParams = defConsParams(rodParams)
 % This function defines the constrained parameters based on the physical
 % struct's boundary conditions. 
 %   Input:
-%       rodParams - define a rod struct (beam is a 2D rod)
+%       rodParams - define a shell struct
 %
 %   Output:
-%       consParams - the constrained parameters contains the constrained
+%       consParams - the constrained parameters contain the constrained
 %       index and unconstrained index of the discrete beam mode
 
 % Define fixed DOF
