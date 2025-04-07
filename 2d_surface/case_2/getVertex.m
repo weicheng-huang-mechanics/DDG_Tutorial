6732a6a975dd68afd9a4c43e7ddaf2dae436e07d
Weicheng Huang <164727992+weicheng-huang-mechanics@users.noreply.github.com>
2025-04-07 20:20:05 +0100

    Update getVertex.m

--- 2d_surface/case_2/getVertex.m
+++ 2d_surface/case_2/getVertex.m
@@ -1,5 +1,16 @@
 function localNode = getVertex(x, i)
 
+% This function extracts the 2D position of the i-th node from the global 
+% DOF vector.
+%
+%   Input:
+%       x - Global degrees of freedom vector (2*nv x 1), where each node 
+%           has 2 DOFs
+%       i - Index of the node to extract (1-based)
+%
+%   Output:
+%       localNode - 2x1 vector containing the i-th node's coordinate
+
 localNode = zeros(2,1);
 
 localNode(1) = x(2*(i-1)+1);
