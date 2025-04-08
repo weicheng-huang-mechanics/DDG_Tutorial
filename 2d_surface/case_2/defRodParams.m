65f27c14e7dee7e68624ea532d3d88393e391f93
Weicheng Huang <164727992+weicheng-huang-mechanics@users.noreply.github.com>
2025-04-08 14:02:52 +0100

    Update defRodParams.m

--- 2d_surface/case_2/defRodParams.m
+++ 2d_surface/case_2/defRodParams.m
@@ -2,12 +2,12 @@ function rodParams = defRodParams(node, edge, bend, simParams)
 
 % This function defines a rotational shell struct used for the DDG-based simulation
 %   Input:
-%       node - Nodal coordinates of the rod (nv x 2)
+%       node - Nodal coordinates of the shell (nv x 2)
 %       edge - Edge connectivity list (ne x 2)
 %       bend - Bending element list (nb x 2)
 %       simParams - numerical parameters of the shell
 %   Output:
-%       rodParams - the defined rod struct contains the physical and
+%       rodParams - the defined shell struct contains the physical and
 %       numerical parameters of the simulated system
 
 % Define struct
