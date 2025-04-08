5fff4d955c8b62c77cd175ec96e4c4ed117593b4
Weicheng Huang <164727992+weicheng-huang-mechanics@users.noreply.github.com>
2025-04-08 07:49:45 +0100

    Update getFg.m

--- 2d_surface/case_2/getFg.m
+++ 2d_surface/case_2/getFg.m
@@ -1,11 +1,11 @@
 function [Fg, Jg] = getFg(rodParams)
 
 % This function computes the gravitational force and jacobian of the simulated system.
-% INPUTS: rodParams - the defined rod struct contains the physical and
+% Input:  rodParams - the defined shell struct contains the physical and
 %                     numerical parameters of the simulated system
 %
-% OUTPUTS: Fg - gravitational forces (2*nv x 1)
-%          Jg - gravitational jacobian (2*nv x 2*nv)
+% Output: Fg - gravitational forces (2*nv x 1)
+%         Jg - gravitational jacobian (2*nv x 2*nv)
 
 Fg = rodParams.m .* rodParams.garr;
 Jg = zeros(rodParams.ndof, rodParams.ndof);
