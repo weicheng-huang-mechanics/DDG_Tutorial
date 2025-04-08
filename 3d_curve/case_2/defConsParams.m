95a610c04c82c00152032614aa13063bc255fe4e
Weicheng Huang <164727992+weicheng-huang-mechanics@users.noreply.github.com>
2025-04-08 13:31:24 +0100

    Update defConsParams.m

--- 3d_curve/case_2/defConsParams.m
+++ 3d_curve/case_2/defConsParams.m
@@ -5,8 +5,8 @@ function consParams = defConsParams(rodParams)
 %       rodParams - define a rod struct
 %
 %   Output:
-%       consParams - the constrained parameters contains the constrained
-%       index and unconstrained index of the discrete beam model
+%       consParams - the constrained parameters contain the constrained
+%       index and unconstrained index of the discrete rod model
 
 nv = rodParams.nv;
 ne = rodParams.ne;
