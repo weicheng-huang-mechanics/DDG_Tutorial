15b4ca995ab22227f8c66e34cda3126c678700c4
Weicheng Huang <164727992+weicheng-huang-mechanics@users.noreply.github.com>
2025-04-08 13:11:50 +0100

    Update plotBeam.m

--- 2d_curve/case_1/plotBeam.m
+++ 2d_curve/case_1/plotBeam.m
@@ -1,5 +1,5 @@
 function plotBeam(x)
-% plotBeam Visualizes the beam (rod) by plotting its nodal positions.
+% plotBeam Visualizes the beam by plotting its nodal positions.
 %
 %   Input:
 %       x - Global DOF vector (2*nv x 1)
