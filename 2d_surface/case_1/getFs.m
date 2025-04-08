1ca6ec3d309ac1edbf016ab8fff403789508dec1
Weicheng Huang <164727992+weicheng-huang-mechanics@users.noreply.github.com>
2025-04-08 07:46:40 +0100

    Update getFs.m

--- 2d_surface/case_1/getFs.m
+++ 2d_surface/case_1/getFs.m
@@ -1,12 +1,12 @@
 function [Fs, Js] = getFs(rodParams, sElement)
 
 % This function computes the stretching force and jacobian of the simulated system.
-% INPUTS: rodParams - the defined rotational shell struct contains the physical and
+% Input:  rodParams - the defined rotational shell struct contains the physical and
 %                     numerical parameters of the simulated system
 %         sElement - the stretching element list
 %
-% OUTPUTS: Fs - stretching forces (2*nv x 1)
-%          Js - stretching jacobian (2*nv x 2*nv)
+% Output: Fs - stretching forces (2*nv x 1)
+%         Js - stretching jacobian (2*nv x 2*nv)
 
 
 Fs = zeros(rodParams.ndof, 1);
