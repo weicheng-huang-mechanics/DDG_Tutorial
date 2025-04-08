c6ba1c14980160e8778e7ed7877bdc5cec9c1d6b
Weicheng Huang <164727992+weicheng-huang-mechanics@users.noreply.github.com>
2025-04-08 08:05:49 +0100

    Update getFg.m

--- 3d_surface/case_2/getFg.m
+++ 3d_surface/case_2/getFg.m
@@ -1,10 +1,10 @@
 function [Fg, Jg] = getFg(plateParams)
 % This function computes the gravitational force and jacobian of the simulated system.
-% INPUTS: plateParams - the defined plate struct contains the physical and
+% Input:  plateParams - the defined plate struct contains the physical and
 %                     numerical parameters of the simulated system
 %
-% OUTPUTS: Fg - gravitational forces (ndof x 1)
-%          Jg - gravitational jacobian (ndof x ndof)
+% Output: Fg - gravitational forces (ndof x 1)
+%         Jg - gravitational jacobian (ndof x ndof)
 
 Fg = plateParams.m .* plateParams.garr;
 Jg = zeros(plateParams.ndof, plateParams.ndof);
