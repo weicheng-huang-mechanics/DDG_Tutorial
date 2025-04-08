5afb26e34dea22ac0ee6f1bf9f24ce76b5217ce2
Weicheng Huang <164727992+weicheng-huang-mechanics@users.noreply.github.com>
2025-04-08 08:00:26 +0100

    Update getFs.m

--- 3d_curve/case_3/getFs.m
+++ 3d_curve/case_3/getFs.m
@@ -1,10 +1,10 @@
 function [Fs, Js] = getFs(rodParams, sElement)
-% This function computes the stretching force and jacobian of the simulated system.
-% INPUTS: rodParams - the defined rod struct contains the physical and
+% This function computes the stretching force and jacobian of the simulated system. 
+% Input:  rodParams - the defined rod struct contains the physical and
 %                     numerical parameters of the simulated system
-%         sElement - the stretching element list (ne x 2)
+%         sElement - the stretching element list
 %
-% OUTPUTS: Fs - stretching forces (ndof x 1)
+% Output:  Fs - stretching forces (ndof x 1)
 %          Js - stretching jacobian (ndof x ndof)
 
 Fs = zeros(rodParams.ndof, 1);
