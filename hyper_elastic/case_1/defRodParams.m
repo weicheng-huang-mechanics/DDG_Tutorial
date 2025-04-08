e175d48d013101ecb5e5319cf19b901e0fc2241a
Weicheng Huang <164727992+weicheng-huang-mechanics@users.noreply.github.com>
2025-04-08 08:09:44 +0100

    Update defRodParams.m

--- hyper_elastic/case_1/defRodParams.m
+++ hyper_elastic/case_1/defRodParams.m
@@ -3,10 +3,9 @@ function rodParams = defRodParams(node, edge, simParams)
 %   Input:
 %       node - Nodal coordinates of the rod (nv x 2)
 %       edge - Edge connectivity list (ne x 2)
-%       bend - Bending element list (nb x 2)
-%       simParams - numerical parameters of the rod
+%       simParams - numerical parameters of the cable
 %   Output:
-%       rodParams - the defined rod struct contains the physical and
+%       rodParams - the defined cable struct contains the physical and
 %       numerical parameters of the simulated system
 
 % Define struct
