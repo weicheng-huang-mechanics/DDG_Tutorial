3be8f072aa0cd3144f0882c6573204cf3698c63d
Weicheng Huang <164727992+weicheng-huang-mechanics@users.noreply.github.com>
2025-04-08 08:20:06 +0100

    Update getFg.m

--- soft_robot/case_2/getFg.m
+++ soft_robot/case_2/getFg.m
@@ -1,10 +1,10 @@
 function [Fg, Jg] = getFg(rodParams)
 % This function computes the gravitational force and jacobian of the simulated system.
-% INPUTS: rodParams - the defined rod struct contains the physical and
+% Input:  rodParams - the defined beam struct contains the physical and
 %                     numerical parameters of the simulated system
 %
-% OUTPUTS: Fg - gravitational forces (ndof x 1)
-%          Jg - gravitational jacobian (ndof x ndof)
+% Output: Fg - gravitational forces (ndof x 1)
+%         Jg - gravitational jacobian (ndof x ndof)
 
 Fg = rodParams.m .* rodParams.garr;
 Jg = zeros(rodParams.ndof, rodParams.ndof);
