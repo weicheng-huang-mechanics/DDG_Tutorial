572c1dd1ce7d7d6472b93a8ccc795d4ac921651e
Weicheng Huang <164727992+weicheng-huang-mechanics@users.noreply.github.com>
2025-04-08 14:04:00 +0100

    Update objfun.m

--- hyper_elastic/case_1/objfun.m
+++ hyper_elastic/case_1/objfun.m
@@ -3,7 +3,7 @@ function xUncons = objfun(rodParams, simParams, consParams, sElement)
 %        using Newton's method on unconstrained degrees of freedom.
 %
 %   Input:
-%       rodParams  - Struct containing rod material and state parameters
+%       rodParams  - Struct containing cable material and state parameters
 %       simParams  - Struct with numerical parameters
 %       consParams - Struct with constraints (unconstrained DOF indices)
 %       sElement   - Stretching element definitions
