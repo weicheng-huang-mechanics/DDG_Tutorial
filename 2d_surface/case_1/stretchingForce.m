f02fc95336b7f56209340ea6c02c4e7ff77adc35
Weicheng Huang <164727992+weicheng-huang-mechanics@users.noreply.github.com>
2025-04-08 13:19:17 +0100

    Update stretchingForce.m

--- 2d_surface/case_1/stretchingForce.m
+++ 2d_surface/case_1/stretchingForce.m
@@ -3,7 +3,7 @@ function [dF, dJ] = stretchingForce(node0, node1, l0, r0, EA, nu)
 % This function computes the stretching force and jacobian of a stretching element.
 % Input:  node0 - position of the first node in the stretching element
 %         node1 - position of the second node in the stretching element
-%         l_k - voronoi length of the bending element
+%         l_k - reference length of the stretching element
 %         EA - stretching stiffness
 %
 % Output: dF - stretching forces (4 x 1)
