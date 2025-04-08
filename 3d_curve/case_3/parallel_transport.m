8fb4f1c6f759e6a0d2eb40f609c4f66b553fc477
Weicheng Huang <164727992+weicheng-huang-mechanics@users.noreply.github.com>
2025-04-08 08:00:59 +0100

    Update parallel_transport.m

--- 3d_curve/case_3/parallel_transport.m
+++ 3d_curve/case_3/parallel_transport.m
@@ -3,7 +3,7 @@ function d = parallel_transport(u, t1, t2)
 %   surface or curve, from the local frame defined by tangent vector t1 
 %   to the one defined by t2. It preserves the directional relationship 
 %   of u relative to t1 as it's mapped to the frame of t2.
-%   Inputs:
+%   Input:
 %       u  - 3x1 vector to be transported
 %       t1 - 3x1 unit tangent vector at the starting point
 %       t2 - 3x1 unit tangent vector at the ending point
