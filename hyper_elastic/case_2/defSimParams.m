fb466a3159f8ca643cda287c24ec495b9505ffd1
Weicheng Huang <164727992+weicheng-huang-mechanics@users.noreply.github.com>
2025-04-08 13:48:54 +0100

    Update defSimParams.m

--- hyper_elastic/case_2/defSimParams.m
+++ hyper_elastic/case_2/defSimParams.m
@@ -1,5 +1,5 @@
 function simParams = defSimParams()
-% This function defines a struct that contains the numerical parameters
+% This function defines the struct that contains the numerical parameters
 %   Input:
 %
 %   Output:
