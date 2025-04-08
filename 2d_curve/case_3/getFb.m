4ea8b56ce09cbbd1292048b586cca36cbd11bad6
Weicheng Huang <164727992+weicheng-huang-mechanics@users.noreply.github.com>
2025-04-08 07:41:54 +0100

    Update getFb.m

--- 2d_curve/case_3/getFb.m
+++ 2d_curve/case_3/getFb.m
@@ -1,11 +1,11 @@
 function [Fb, Jb] = getFb(rodParams, bElement)
 % This function computes the bending force and jacobian of the simulated system.
-% INPUTS: rodParams - the defined rod struct contains the physical and
+% Input:  rodParams - the defined rod struct contains the physical and
 %                     numerical parameters of the simulated system
 %         bElement - the bending element list
 %
-% OUTPUTS: Fb - bending forces (2*nv x 1)
-%          Jb - bending jacobian (2*nv x 2*nv)
+% Output: Fb - bending forces (2*nv x 1)
+%         Jb - bending jacobian (2*nv x 2*nv)
 
 Fb = zeros(rodParams.ndof, 1);
 Jb = zeros(rodParams.ndof, rodParams.ndof);
